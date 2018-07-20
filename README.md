a343a602198c555e780d3dd2051efaf6b8921930
afeiship <1290657123@qq.com>
2018-07-20 14:47:37 +0800

    issue: intail

---
+++ README.md
@@ -0,0 +1,2 @@
+# webpack-app-kits
+> Webpack application kits
---
+++ src/loaders/css-loader.js
@@ -0,0 +1,14 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CSS_RE = /\.css$/;
+
+module.exports = {
+  test: CSS_RE,
+  use: ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: [
+      'css-hot-loader',
+      'css-loader',
+      'postcss-loader'
+    ]
+  })
+};
