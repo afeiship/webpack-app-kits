66b8ea740460ed5dfea3f93403de1f06bdd4c3c3
afeiship <1290657123@qq.com>
2018-07-20 14:54:07 +0800

    issue: everything is array

--- libs/loaders/css-loader.js
+++ libs/loaders/css-loader.js
@@ -1,14 +1,16 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CSS_RE = /\.css$/;
 
-module.exports = {
-  test: CSS_RE,
-  use: ExtractTextPlugin.extract({
-    fallback: 'style-loader',
-    use: [
-      'css-hot-loader',
-      'css-loader',
-      'postcss-loader'
-    ]
-  })
-};
+module.exports = [
+  {
+    test: CSS_RE,
+    use: ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: [
+        'css-hot-loader',
+        'css-loader',
+        'postcss-loader'
+      ]
+    })
+  }
+];
