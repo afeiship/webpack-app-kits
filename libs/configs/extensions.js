2260f97a47d7113e10d59d4c29d70798bab4b88c
afeiship <1290657123@qq.com>
2018-07-23 13:20:30 +0800

    issue: udpate

--- libs/configs/extensions.js
+++ libs/configs/extensions.js
@@ -2,10 +2,5 @@ const merge = require('../utils/merge');
 const DEFAULT_EXTS = ['.js', '.json', '.scss', '.css'];
 
 module.exports = function (inOptions) {
-  if (inOptions) {
-    return merge([
-      DEFAULT_EXTS, inOptions
-    ]);
-  }
-  return DEFAULT_EXTS;
+  return inOptions || DEFAULT_EXTS;
 };
