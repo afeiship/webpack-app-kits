d7a0ccd0ecb3b8206490e2ab6213b77c391611a0
afeiship <1290657123@qq.com>
2019-03-29 14:54:52 +0800

    add: progressbar plugin

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ module.exports = {
     loaderOptions: require('./lib/plugins/loader-options'),
     moduleConcatenation: require('./lib/plugins/module-concatenation'),
     multipleHtml: require('./lib/plugins/multiple-html'),
+    progressBar: require('./lib/plugins/progress-bar'),
     provide: require('./lib/plugins/provide'),
     resourceHints: require('./lib/plugins/resource-hints'),
     semver: require('./lib/plugins/semver'),
